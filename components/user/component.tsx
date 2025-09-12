09296a79858464f26b086980d2fd3fa18e4c7561
richisf <saezferezr@gmail.com>
2025-09-12 22:35:57 +0100

    padding

--- components/user/component.tsx
+++ components/user/component.tsx
@@ -26,7 +26,7 @@ export function User() {
           }}
         >
           {isMobile ? (
-            <div className="flex items-center justify-center h-full">
+            <div className="p-4 flex items-center justify-center h-full">
               <Image
                 src="/name.svg"
                 alt="White Node Logo"
